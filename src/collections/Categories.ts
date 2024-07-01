2b4bc505ce38f45c2b3276a405575490af35a061
delavinamedia <koeppen.h@gmail.com>
2024-07-01 18:11:29 +0200

    categories collection added

---
+++ src/collections/Categories.ts
@@ -0,0 +1,29 @@
+import type { CollectionConfig } from 'payload'
+
+const Categories: CollectionConfig = {
+  slug: 'categories',
+  admin: {
+    useAsTitle: 'title',
+  },
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'media',
+      type: 'upload',
+      relationTo: 'media',
+    },
+  ],
+}
+
+export default Categories
