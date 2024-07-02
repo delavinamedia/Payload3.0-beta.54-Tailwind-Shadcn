ba0cc8150ca7e264d7c740eae118c69c290d77c9
delavinamedia <koeppen.h@gmail.com>
2024-07-02 14:58:12 +0200

    Update page.tsx

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HeaderNavBar from '@/components/app/NavBar'
+import Dummy from '@/components/app/Dummy'
 
 const home = () => {
   return (
     <div className="mx-auto flex w-full flex-col justify-start">
       <HeaderNavBar />
+      <main className="flex min-h-screen flex-col gap-4 py-4 lg:container ">
+        <Dummy />
+      </main>
     </div>
   )
 }
