e07b92c667fd23b814391ce4d06ada9e9d2dd853
delavinamedia <koeppen.h@gmail.com>
2024-07-02 12:01:58 +0200

    Create themeToggle.tsx

---
+++ src/components/themeToggle.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import * as React from 'react'
+import { useTheme } from 'next-themes'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { SunIcon, MoonIcon, MonitorIcon } from '../components/icons'
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          {theme === 'light' && <SunIcon className="h-[1.5rem] w-[1.3rem] " />}
+          {theme === 'dark' && <MoonIcon className="h-5 w-5" />}
+          {theme === 'system' && <MonitorIcon className="h-5 w-5" />}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
